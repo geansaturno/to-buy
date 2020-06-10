c3394f94968ddafaac54054905a96490e1c48d64
Gean Saturno <gsgoncalves@uolinc.com>
2020-06-10 20:56:53 -0300

    Teste do botão

--- src/components/ToDoButton.vue
+++ src/components/ToDoButton.vue
@@ -1,10 +1,19 @@
 <template>
-    <div></div>
+    <button @click="action.call()">{{text}}</button>
 </template>
 
 <script>
 export default {
-
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    action: {
+      type: Function,
+      required: true
+    }
+  }
 }
 
 </script>
--- tests/unit/components/ToDoButton.spec.js
+++ tests/unit/components/ToDoButton.spec.js
@@ -1,12 +1,35 @@
 import ToDoButton from '@/components/ToDoButton.vue'
 import { shallowMount } from '@vue/test-utils'
 import { expect } from 'chai'
+import { fake } from 'sinon'
+
+function buttomFactory (text, action = fake()) {
+  return shallowMount(ToDoButton, {
+    propsData: {
+      text,
+      action
+    }
+  })
+}
 
 describe('ToDoButton', () => {
   it('Deve ser um botão', () => {
-    const cp = shallowMount(ToDoButton)
+    const cp = buttomFactory('Fechar')
 
-    expect(cp.element).to.be.equal('button')
+    expect(cp.element.tagName.toLocaleLowerCase()).to.be.equal('button')
   })
-  it('Deve ter o texto Clique Aqui')
+
+  it('Deve ter o texto "Clique Aqui"', () => {
+    const cp = buttomFactory('Clique Aqui')
+    expect(cp.text()).to.be.equal('Clique Aqui')
+  })
+
+  it('Deve executar uma ação quando for clicado', () => {
+    const acao = fake()
+    const cp = buttomFactory('Fechar', acao)
+
+    cp.trigger('click')
+
+    expect(acao.called).to.be.true
+  })
 })
