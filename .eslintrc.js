d3cb45c015a64b273a07b504cae6b1dd62b691ed
Gean Saturno <gsgoncalves@uolinc.com>
2020-06-10 20:56:46 -0300

    Plugin chai para o eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,19 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    "plugin:chai-friendly/recommended"
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
+  plugins: [
+    'chai-friendly'
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "chai-friendly/no-unused-expressions": 2
   },
   overrides: [
     {
